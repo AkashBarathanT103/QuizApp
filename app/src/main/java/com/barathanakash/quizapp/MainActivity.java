205698ffb6dc3882ffe899230f44bdd616af6c7c
Akash <tjepicdude@gmail.com>
2020-02-04 10:04:14 -0500

    hi

--- app/src/main/java/com/barathanakash/quizapp/MainActivity.java
+++ app/src/main/java/com/barathanakash/quizapp/MainActivity.java
@@ -8,11 +8,13 @@ import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.media.MediaPlayer;
 
 public class MainActivity extends AppCompatActivity {
     Button submitButton;
     EditText responseText;
     TextView displayText;
+    final MediaPlayer bgm =MediaPlayer.create(this, R.raw.music);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
         responseText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus){
@@ -39,7 +42,18 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+
+        Button playMusic = (Button) this.findViewById(R.id.soundButton);
+        playMusic.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                bgm.start();
+            }
+        });
         }
+
+
 
     }
 
